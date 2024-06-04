b72b0c0d3b01da8e4d552e8fdc9fad0bb1d629bf
gaohengyi <gaohengyi@yacloud.net>
2024-06-04 14:15:27 +0800

    update cf dockerfile

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo y | warp-cli registration new && warp-cli mode proxy && warp-cli connect
+dbus-daemon --system --fork \
+    && /usr/bin/warp-svc \
+    && echo y | warp-cli registration new \
+    && warp-cli mode proxy \
+    && warp-cli connect \
+    && /caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
 
-/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
