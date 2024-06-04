444092a2e7e6185cd7424e3b6c82577c9f7ebf06
gaohengyi <gaohengyi@yacloud.net>
2024-06-04 15:16:13 +0800

    update cf dockerfile

--- cmd.sh
+++ cmd.sh
@@ -6,13 +6,10 @@ dbus-daemon --system --fork \
 
 /usr/bin/warp-svc &
 
-sleep 3
+sleep 5
 
-warp-cli --accept-tos registration new
-
-warp-cli --accept-tos mode proxy
-
-warp-cli --accept-tos connect
-
-/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+warp-cli --accept-tos registration new \
+    && warp-cli --accept-tos mode proxy \
+    && warp-cli --accept-tos connect \
+    && /caddy run --config /Caddyfile --adapter caddyfile
 
