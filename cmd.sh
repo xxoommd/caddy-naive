40b688814819a69424970ca458f8fed27af473a5
gaohengyi <gaohengyi@yacloud.net>
2024-06-04 14:39:01 +0800

    update cf dockerfile

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /run/dbus \
     && dbus-daemon --system --fork \
-    && nohup /usr/bin/warp-svc & .
+    && nohup /usr/bin/warp-svc
     #  \
     # & warp-cli --accept-tos registration new \
     # && warp-cli --accept-tos mode proxy \
